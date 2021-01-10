f44e69cd850f84ae173840ee0e526c4c9cf7d1b1
massimoImpulso <46138806+massimoImpulso@users.noreply.github.com>
2021-01-10 18:42:44 +0100

    Update Natura.php

--- src/Codifiche/Natura.php
+++ src/Codifiche/Natura.php
@@ -28,12 +28,11 @@ abstract class Natura
 
     protected static $codifiche = array(
         'N1' => 'escluse ex art. 15',
-        'N2' => 'non soggette',
-        'N3' => 'non imponibili',
+        'N2.1' => 'non soggette artt 7 - 7septies',
+        'N3.4' => 'non imponibili assimilate a cessioni per esportazione',
         'N4' => 'esenti',
         'N5' => 'regime del margine / IVA non esposta in fattura',
-        'N6' => 'inversione contabile (per le operazioni in reverse charge ovvero nei casi di autofatturazione per 
-        acquisti extra UE di servizi ovvero per importazioni di beni nei soli casi previsti)',
+        'N6.9' => 'inversione contabile - altri casi',
         'N7' => 'IVA assolta in altro stato UE (vendite a distanza ex art. 40 c. 3 e 4 e art. 
         41 c. 1 lett. b,  DL 331/93; prestazione di servizi di telecomunicazioni, tele-radiodiffusione 
         ed elettronici ex art. 7-sexies lett. f, g, art. 74-sexies DPR 633/72)'
